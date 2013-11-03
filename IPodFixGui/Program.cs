2057360c5989ea62bb63e49d7092e2d5ffa01e0e
Matt Memmesheimer <m.memmesheimer@gmail.com>
2013-11-02 23:55:07 -0500

    Added refactored form file.

--- IPodFixGui/Program.cs
+++ IPodFixGui/Program.cs
@@ -15,7 +15,7 @@ namespace IPodFixGui
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new IPodFixGui());
         }
     }
 }
