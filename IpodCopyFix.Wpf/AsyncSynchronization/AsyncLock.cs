b9c5921bfebdd52d6289f37a3d82a564000f1751
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-25 12:02:34 -0500

    Added implementations of lock and semaphore that can be used with the asnyc/await keywords.

---
+++ IpodCopyFix.Wpf/AsyncSynchronization/AsyncLock.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IpodCopyFix.Wpf.AsyncSynchronization
+{
+    /// <summary>
+    /// A lock that can be used with the async/await keywords.
+    /// </summary>
+    public class AsyncLock
+    {
+        #region Constants
+
+        private const int LockCount = 1;
+
+        #endregion
+
+        /// <summary>
+        /// Constructs a new <see cref="AsyncLock"/>.
+        /// </summary>
+        public AsyncLock()
+        {
+            _semaphore = new AsyncSemaphore(LockCount);
+            _releaser = Task.FromResult(new Releaser(this));
+        }
+
+        /// <summary>
+        /// Asynchronously waits for access to the lock.
+        /// </summary>
+        /// <returns></returns>
+        public Task<Releaser> LockAsync()
+        {
+            var wait = _semaphore.WaitAsync();
+            return wait.IsCompleted
+                ? _releaser
+                : wait.ContinueWith((_, state) => new Releaser((AsyncLock) state), this,
+                    CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+        }
+
+        #region Fields
+
+        private readonly AsyncSemaphore _semaphore;
+        private readonly Task<Releaser> _releaser;
+
+        #endregion
+
+        #region Releaser
+
+        /// <summary>
+        /// Struct implementing <see cref="IDisposable"/> to allow us to use the 
+        /// <see cref="AsyncLock"/> within a "using" statement.
+        /// </summary>
+        public struct Releaser : IDisposable
+        {
+            internal Releaser(AsyncLock lockToRelease)
+            {
+                _lock = lockToRelease;
+            }
+
+            /// <see cref="IDisposable.Dispose"/>
+            public void Dispose()
+            {
+                if (_lock != null)
+                {
+                    _lock._semaphore.Release();
+                }
+            }
+
+            #region Fields
+
+            private readonly AsyncLock _lock;
+
+            #endregion
+        }
+
+        #endregion
+    }
+}
---
+++ IpodCopyFix.Wpf/AsyncSynchronization/AsyncSemaphore.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IpodCopyFix.Wpf.AsyncSynchronization
+{
+    /// <summary>
+    /// A sempahore that can be used with the async/await keywords.
+    /// </summary>
+    public class AsyncSemaphore
+    {
+        #region Constants
+
+        private static readonly Task CompletedTask = Task.FromResult(true);
+
+        #endregion
+
+        /// <summary>
+        /// Constructs a new <see cref="AsyncSemaphore"/> with the specified initial count.
+        /// </summary>
+        /// <param name="count">Initial count of the semaphore.</param>
+        public AsyncSemaphore(int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentException(@"count must be greater than 0", "count");
+            }
+            _count = count;
+            _waiters = new Queue<TaskCompletionSource<bool>>();
+        }
+
+        /// <summary>
+        /// Asynchronously waits for access to the semaphore.
+        /// </summary>
+        /// <returns>An awaitable task allowing access to the semaphore.</returns>
+        public Task WaitAsync()
+        {
+            lock (_waiters)
+            {
+                if (_count > 0)
+                {
+                    --_count;
+                    return CompletedTask;
+                }
+                var waiter = new TaskCompletionSource<bool>();
+                _waiters.Enqueue(waiter);
+                return waiter.Task;
+            }
+        }
+
+        /// <summary>
+        /// Releases the sempahore.
+        /// </summary>
+        public void Release()
+        {
+            TaskCompletionSource<bool> toRelease = null;
+            lock (_waiters)
+            {
+                if (_waiters.Count > 0)
+                {
+                    toRelease = _waiters.Dequeue();
+                }
+                else
+                {
+                    ++_count;
+                }
+                if (toRelease != null)
+                {
+                    toRelease.SetResult(true);
+                }
+            }
+        }
+
+        #region Fields
+
+        private int _count;
+        private readonly Queue<TaskCompletionSource<bool>> _waiters;
+
+        #endregion
+    }
+}
