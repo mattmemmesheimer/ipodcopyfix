be6631b033fe513858c024748606f6c4a72dcfac
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-24 22:19:39 -0500

    Added a view model base class.

---
+++ IpodCopyFix.Wpf/ViewModels/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IpodCopyFix.Wpf.ViewModels
+{
+    /// <summary>
+    /// Base class for view models.
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        #region Events
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #endregion
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
