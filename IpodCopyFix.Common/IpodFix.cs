7e3e08bef9f566f828fcedca437b95026c58c4df
mattmemmesheimer <m.memmesheimer@gmail.com>
2015-07-26 19:56:02 -0500

    Split up the fix directory method.

--- IpodCopyFix.Common/IpodFix.cs
+++ IpodCopyFix.Common/IpodFix.cs
@@ -4,7 +4,6 @@ using System.Threading.Tasks;
 using IpodCopyFix.Common.AsyncSynchronization;
 using IpodCopyFix.Common.Util;
 using log4net;
-using File = TagLib.File;
 
 namespace IpodCopyFix.Common
 {
@@ -47,22 +46,8 @@ namespace IpodCopyFix.Common
             int skipped = 0;
             foreach (var file in files)
             {
-                var tag = File.Create(file);
-                var artist = tag.Tag.FirstAlbumArtist;
-                if (!string.IsNullOrEmpty(artist))
-                {
-                    artist = artist.RemoveIllegalFilenameChars();
-                    using (await _lock.LockAsync())
-                    {
-                        // create directory if it doesn't already exist
-                        var dir = Path.Combine(_destinationPath, artist);
-                        if (!Directory.Exists(dir))
-                        {
-                            Directory.CreateDirectory(dir);
-                        }
-                    }
-                }
-                else
+                var result = await FixFileAsync(file);
+                if (result)
                 {
                     skipped++;
                     Logger.DebugFormat("Skipping blank FirstArtist.");
@@ -70,6 +55,30 @@ namespace IpodCopyFix.Common
             }
             Logger.DebugFormat("Finished fixing directory {0}.  Skipped {1} of {2} files.", path,
                 skipped, files.Length);
+        }
+
+        private async Task<bool> FixFileAsync(string path)
+        {
+            var tag = TagLib.File.Create(path);
+            var artist = tag.Tag.FirstAlbumArtist;
+            if (!string.IsNullOrEmpty(artist))
+            {
+                artist = artist.RemoveIllegalFilenameChars();
+                using (await _lock.LockAsync())
+                {
+                    // create directory if it doesn't already exist
+                    var dir = Path.Combine(_destinationPath, artist);
+                    if (!Directory.Exists(dir))
+                    {
+                        Directory.CreateDirectory(dir);
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
         }
 
         #region Fields
